537828474980308102582a78f5bf3c795605e07e
JuanEstebanR <juan1010.jerm@gmail.com>
2024-09-04 15:06:00 -0500

    Added filter to the API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status
+from rest_framework import status, filters
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -135,3 +135,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = UserProfile.objects.all()
     authentication_classes = (TokenAuthentication, )
     permission_classes = (UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email')
